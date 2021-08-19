c2d451498c7245be6f9af2bc7aada0d87f3caae8
Vadim Belevtsov <vadim.belov36@gmail.com>
2021-08-19 15:44:06 +0300

    Refactoring

--- client.py
+++ client.py
@@ -1,14 +1,102 @@
+import logging
 import socket
-import pickle
-import struct
+import _pickle as cpk
+import _struct as cstruct
+import zlib
+import time
+import multiprocessing
+from multiprocessing import Queue
 
-from PIL import ImageGrab
+import mss
+from mss.base import ScreenShot
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 8089))
 
-while True:
-    img = ImageGrab.grab(bbox=(100, 50, 1920, 1280))
-    data = pickle.dumps(img)
-    message_size = struct.pack("L", len(data))
-    clientsocket.sendall(message_size + data)
+class Client:
+
+    @staticmethod
+    def send_frame_to_socket(client, buffer):
+        while True:
+            if buffer.qsize() >= 10:
+                data = buffer.get(block=False)
+                print("Sending data...")
+                client.sendall(data)
+            else:
+                time.sleep(0.01)
+
+
+class DataConverter:
+
+    @staticmethod
+    def compress_data(data: bytes) -> bytes:
+        return zlib.compress(data, level=1)
+
+    @staticmethod
+    def struct_data(compress_data: bytes) -> bytes:
+        return cstruct.pack("L", len(compress_data))
+
+    @staticmethod
+    def convert_img_to_bytes(img: ScreenShot):
+        return cpk.dumps(img)
+
+    @staticmethod
+    def get_img_from_queue(queue: Queue):
+        while True:
+            if queue.qsize() >= 10:
+                return queue.get(block=False)
+            else:
+                time.sleep(0.01)
+
+    def run(self, images: Queue, buffer: Queue):
+        while True:
+            now = time.time()
+            img = self.get_img_from_queue(images)
+            print("Img recv")
+            bytes_img = self.convert_img_to_bytes(img)
+            compress_data = self.compress_data(bytes_img)
+            message_size = self.struct_data(compress_data)
+            print("Data sending to buffer...")
+            buffer.put(message_size + compress_data, block=False)
+            print(time.time() - now)
+
+
+class ScreenShotGenerator:
+    @staticmethod
+    def put_screenshot_to_buffer(queue: Queue):
+        src = mss.mss()
+        monitor = {'top': 0, 'left': 0, 'width': 720, 'height': 480}
+        while True:
+            img = src.grab(monitor)
+            print("Img sending to queue...")
+            queue.put(img, block=False)
+
+
+if __name__ == '__main__':
+    m = multiprocessing.Manager()
+    queue_with_img = m.Queue()
+    queue_with_data = m.Queue()
+    client_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_conn.connect(('localhost', 8089))
+
+    processes = []
+
+    ssg = ScreenShotGenerator()
+    screen_shot_gen_proc = multiprocessing.Process(target=ssg.put_screenshot_to_buffer, args=(queue_with_img,))
+    screen_shot_gen_proc.start()
+    processes.append(screen_shot_gen_proc)
+    print("Screenshot started...")
+
+    converter = DataConverter()
+    for i in range(2):
+        print(f"Converted {i} started...")
+        process = multiprocessing.Process(target=converter.run, args=(queue_with_img, queue_with_data))
+        processes.append(process)
+        process.start()
+
+    client = Client()
+    client_process = multiprocessing.Process(target=client.send_frame_to_socket, args=(client_conn, queue_with_data,))
+    client_process.start()
+    processes.append(client_process)
+    print(f"Client started...")
+
+    for process in processes:
+        process.join()
--- server.py
+++ server.py
@@ -1,24 +1,14 @@
-import pickle
+import _pickle as pickle
 import socket
-import struct
+import _struct as struct
 import time
-from multiprocessing import Process, Manager
-from multiprocessing import Queue
+import zlib
 
 import cv2
 import numpy as np
 
 
-def buffer_count(buffer: Queue):
-    while True:
-        if buffer.qsize() >= 5000:
-            frame = buffer.get(block=False)
-            time.sleep(0.5)
-            cv2.imshow('frame', frame)
-            cv2.waitKey(1)
-
-
-def frame_executor(buffer: Queue):
+def frame_executor():
     HOST = ''
     PORT = 8089
 
@@ -36,37 +26,28 @@ def frame_executor(buffer: Queue):
     payload_size = struct.calcsize("L")
 
     while True:
+        now = time.time()
         while len(data) < payload_size:
-            data += conn.recv(4096)
+            data += conn.recv(4096*30)
 
         packed_msg_size = data[:payload_size]
         data = data[payload_size:]
         msg_size = struct.unpack("L", packed_msg_size)[0]
 
         while len(data) < msg_size:
-            data += conn.recv(4096)
+            data += conn.recv(4096*30)
 
         frame_data = data[:msg_size]
         data = data[msg_size:]
-        frame = pickle.loads(frame_data)
+        try:
+            frame = pickle.loads(zlib.decompress(frame_data))
+        except Exception as e:
+            print(e)
         d = np.array(frame)
-        buffer.put(d)
-        print(buffer.qsize())
+        cv2.imshow('frame', d)
+        cv2.waitKey(1)
+        print(time.time() - now)
 
 
 if __name__ == '__main__':
-
-    processes = []
-    m = Manager()
-    buffer = m.Queue()
-    proc1 = Process(target=frame_executor, args=(buffer,))
-    proc2 = Process(target=buffer_count, args=(buffer,))
-
-    processes.append(proc2)
-    processes.append(proc1)
-
-    proc1.start()
-    proc2.start()
-
-    for proc in processes:
-        proc.join()
+    frame_executor()
